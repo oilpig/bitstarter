f4d97ec68280d0594abd070323561380e4b26430
oilpig <aoyuzhang1989@gmail.com>
2013-07-14 01:34:58 +0000

    bitstarter web js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(20);
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  	fs.readFileSync(buffer,"index.html");
+	response.send(Buffer.toString(buffer));
 });
 
 var port = process.env.PORT || 5000;
